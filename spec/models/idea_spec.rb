57a5b10fe187f2857f87c048e93a9feadc5de7e1
George Schena <gs14aaa@herts.ac.uk>
2018-06-24 22:08:15 +0100

    Task 2/3 completed

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Idea, type: :model do
+  it "has a name" do 
+    idea = Idea.create!(name: "My Awesome Idea Name") 			# creating a new idea 'instance'
+    second_idea = Idea.create!(name: "My Second Idea Name") 	# creating another new idea 'instance'
+    expect(second_idea.name).to eq("My Second Idea Name") 		# this is our expectation
+  end
+
+  it "has a description" do 
+  	description = Idea.create!(description: "My description")
+  	another_description = Idea.create!(description: "Another description")
+  	expect(another_description.description).to eq("Another description")
+  end 
+end
