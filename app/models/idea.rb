5427e079bb20a948c0475804459f54b5ec7a49e2
George Schena <gs14aaa@herts.ac.uk>
2018-06-24 23:46:09 +0100

    task 5 is completed

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,6 @@
 class Idea < ApplicationRecord
 	mount_uploader :picture, PictureUploader
+	has_many :comments
+	validates :name, presence: true
+ 	validates :description, presence: true
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -2,14 +2,25 @@ require "rails_helper"
 
 RSpec.describe Idea, type: :model do
   it "has a name" do 
-    idea = Idea.create!(name: "My Awesome Idea Name") 			# creating a new idea 'instance'
-    second_idea = Idea.create!(name: "My Second Idea Name") 	# creating another new idea 'instance'
+    idea = Idea.create(name: "My Awesome Idea Name") 			# creating a new idea 'instance'
+    second_idea = Idea.create(name: "My Second Idea Name") 	# creating another new idea 'instance'
     expect(second_idea.name).to eq("My Second Idea Name") 		# this is our expectation
   end
 
   it "has a description" do 
-  	description = Idea.create!(description: "My description")
-  	another_description = Idea.create!(description: "Another description") 
-  	expect(another_description.description).to eq("Another description")
+  	idea = Idea.create(description: "My description")
+  	second_idea = Idea.create(description: "second_description")
+  	expect(second_idea.description).to eq("second_description")
   end 
+
+  describe "associations" do
+    it{ is_expected.to have_many(:comments) }
+  end
+
+  describe "validations" do
+    it{ is_expected.to validate_presence_of :name }
+    it{ is_expected.to validate_presence_of :description }
+  end
 end
+
+
